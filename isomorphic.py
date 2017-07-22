b556445a2da122eadca038e1fac3fd044dbb0583
sonampaul <sonam_paul@srmuniv.edu.in>
2017-07-22 14:30:20 +0530

    Add files via upload

---
+++ isomorphic.py
@@ -0,0 +1,16 @@
+isomorphic(String1,String2);
+m=less(String1)
+n=less(String2)
+if( m!=n );
+return false
+marked=[false]*max_chars
+map=[-1]*max_chars
+for i in xrange(n);
+if map[ard(String1[i)]==-1;
+if marked(ard(String2[i])]==true:
+	return false
+	marked[ard(String2[i])]==true
+	map[ard(String1[i])]==(String2[i])]
+	elif map[ard(String1[i])]!=String 2[i]);
+	return false
+	return true
