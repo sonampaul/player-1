a4f22641bf7bb3645ea40fe2d246f4cc8285415c
sonampaul <sonam_paul@srmuniv.edu.in>
2017-07-22 14:03:43 +0530

    Add files via upload

---
+++ star.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		/	int i, space, rows, k=0;
+		Scanner scan=new Scanner(System.in);
+		System.out.print("enter no of rows : ");
+		rows=scan.nextInt();
+ 
+{
+ 
+ 
+		for(space=1; space<=(rows-1); space++)
+			{
+				System.out.print(" ");
+			}
+			while(k !=(2*i-1))
+			{
+				System.out.println("*");
+				k++;
+			}
+			k=0;
+			System.out.println();
+	}
+}
