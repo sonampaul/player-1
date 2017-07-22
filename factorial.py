ddb2899db25d1db40ebf6c4df44628acb806bead
sonampaul <sonam_paul@srmuniv.edu.in>
2017-07-22 10:41:29 +0530

    Add files via upload

---
+++ factorial.py
@@ -0,0 +1,11 @@
+num=7
+num=int(input("enter a number:"))
+factorial=1
+if num<0
+print("sorry, factorial doesnot exist for negative numbers")
+elif num=0:
+	print("the factorial list of 0 is 1"
+	else
+	for i in range(1,num+1):
+		factorial=factorial*i
+		print("the factorial of",num,"is",factorial)
